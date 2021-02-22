79001cb49c994f759851fad48ee8fb9d2fb431b3
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-02-22 09:56:02 +0330

    Add Business Category Icon

--- DomainClass/Category.cs
+++ DomainClass/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainClass
 {
@@ -12,6 +13,8 @@ namespace DomainClass
 		[MaxLength(50 , ErrorMessage ="طول نام بیش از حد زیاد است ")]
 		public string Name { get; set; }
 		public int? ParentCategoryId { get; set; }
+		[Column(TypeName = "varchar(50)")]
+		public string Icon { get; set; }
 		public virtual Category ParentCategory { get; set; }
 		public virtual ICollection<CategoryFeature> CategoryFeatures { get; set; }
 	}
