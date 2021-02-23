7ce5bb9dff6b2ff52b80fad3f7dd093223aedc1e
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-02-23 17:16:37 +0330

    Add Other

---
+++ BizApp/Areas/Admin/Models/CreateUpdateMainCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BizApp.Areas.Admin.Models
+{
+	public class CreateUpdateMainCategory
+	{
+		public int CategoryId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		//public int? ParentCategoryId { get; set; }
+		public string Icon { get; set; }
+		public int Order { get; set; }
+	}
+}
---
+++ DomainClass/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace DomainClass.Commands
+{
+	public class CreateCategoryCommand
+	{
+		public string Name { get; set; }
+		public string Icon { get; set; }
+		public int Order { get; set; }
+	}
+}
