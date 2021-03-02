c151b91a6a0ca4700785c9b097dd7f22de09fadc
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-03-02 16:06:03 +0330

    Complete Register

---
+++ BizApp/Extensions/UniqueMemberMobileAttribute.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BizApp.Extensions
+{
+	public class UniqueMemberMobileAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+			var entity = _context.Users.SingleOrDefault(e => e.Mobile == Convert.ToInt64(value));
+
+			if (entity != null)
+			{
+				return new ValidationResult(GetErrorMessage(value.ToString()));
+			}
+			return ValidationResult.Success;
+		}
+
+		public string GetErrorMessage(string email)
+		{
+			return $"شماره موبایل تکراری است";
+		}
+	}
+}
---
+++ BizApp/Extensions/UniqueUserNameAttribute.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BizApp.Extensions
+{
+	public class UniqueUserNameAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+			var entity = _context.Users.SingleOrDefault(e => e.UserName == value.ToString());
+
+			if (entity != null)
+			{
+				return new ValidationResult(GetErrorMessage(value.ToString()));
+			}
+
+			return ValidationResult.Success;
+		}
+
+		public string GetErrorMessage(string email)
+		{
+			return $"نام کاربری تکراری است";
+		}
+	}
+}
