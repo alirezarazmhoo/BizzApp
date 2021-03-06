30501326e1dd39f7df81b71fb5ffcb2f23f6e8fe
alirezarazmjoo <alireza1539razmjoo@yahoo.com>
2021-03-06 10:52:05 +0330

    Add Models UsersInCustomerBusinessMediaLikes And CustomerBusinessMedias

---
+++ DomainClass/Review/UsersInCustomerBusinessMediaLike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainClass.Review
+{
+public	class UsersInCustomerBusinessMediaLike
+	{
+		public Guid Id { get; set; }
+		public string BizAppUserId { get; set; }
+		public BizAppUser BizAppUser { get; set; }
+		public Guid CustmoerBusinessMediaId { get; set; }
+		public CustomerBusinessMedia  CustomerBusinessMedia { get; set; }
+	}
+}
