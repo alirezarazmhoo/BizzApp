5121e55e2b2268ddcd084cda548633424948ca58
alirezarazmjoo <alireza1539razmjoo@yahoo.com>
2021-03-14 14:43:25 +0330

    Add Features To RecentActivity

--- BizApp/wwwroot/Scripts/MainWebSite/Index/Index.js
+++ BizApp/wwwroot/Scripts/MainWebSite/Index/Index.js
@@ -1,5 +1,4 @@
-﻿//import { read } from "fs/promises";
-
+﻿
 function AutoComplete(parameter) {
 	var input = document.getElementById('idTxtCategorySearch');
 	if (input.value.length == 0) {
@@ -65,7 +64,8 @@ function FillCategorySearchInput(listname) {
 function GetMoreActivity() {
 	var personActivityType = "";
 	var businessPicture = ""; 
-	
+	var count = 0; 
+	var img = [];
 	$.ajax({
 		type: "GET",
 		url: 'Home/GetMoreAcivites',
@@ -73,9 +73,6 @@ function GetMoreActivity() {
 		contentType: false,
 		processData: false,
 		success: function (response) {
-			//alert(JSON.stringify(response.items));
-			//alert(response.items[0].mainPage_RecentActivityContent.name)
-		
 			$.each(response.items, function () {
 				if (this.activityType == 1) {
 					personActivityType = "یک نظر ثبت کرد";
@@ -92,11 +89,12 @@ function GetMoreActivity() {
 						RecentActivityBusinessMediaSignlePictureCreator(this.mainPage_RecentActivityCreator.name, this.mainPage_RecentActivityCreator.image, this.mainPage_RecentActivityContent.name, this.mainPage_RecentActivityUserMediaBusinesses[0].image, this.mainPage_RecentActivityContent.text, this.mainPage_RecentActivityContent.rate);
 					}
 					else {
-						for (var i = 0; i < this.mainPage_RecentActivityUserMediaBusinesses.length; i++) {
-							alert(this.mainPage_RecentActivityUserMediaBusinesses[i].image); 
+						for (var i = 1; i < this.mainPage_RecentActivityUserMediaBusinesses.length; i++) {
+							count++;
+							img.push(this.mainPage_RecentActivityUserMediaBusinesses[i].image);
 						}
+						RecentActivityBusinessMediaMultiPictureCreator(this.mainPage_RecentActivityCreator.image, this.mainPage_RecentActivityCreator.name, this.mainPage_RecentActivityContent.name, this.mainPage_RecentActivityUserMediaBusinesses[0].image, this.mainPage_RecentActivityContent.rate, this.mainPage_RecentActivityContent.text, count, img, this.mainPage_RecentActivityContent.text, this.mainPage_RecentActivityContent.businessimg);
 					}
-
 				}			
 			});
 		
@@ -117,9 +115,9 @@ function RecentActivityCreator(personIcon, personName, personActivityType, busin
 	$masonry.masonry('layout');
 
 }
-function RecentActivityBusinessMediaSignlePictureCreator(personName, personIcon, businessName , media ,businessDesc ,rate) {
+function RecentActivityBusinessMediaSignlePictureCreator(personName, personIcon, businessName, media, businessDesc, rate, count, description) {
 
-	var $data = $('<div class="recent-activities__col"> <div class="recent-activities__item2"> <div class="recent-activities__item2__title"> <div class="recent-activities__item2__title__image"> <a href="#"> <img src="' + personIcon + '"></a> </div> <div class="recent-activities__item2__title__text"> <a href="#">' + personName + '</a> <span>یک عکس اضافه کرد</span> </div> </div> <div class="recent-activities__item2__content"> <div class="recent-activities__item2__content__title"> <a href="#"> <span>  ' + businessName + ' </span></a> <div class="recent-activities__item2__content__title__popover"> <div class="recent-activities__item2__content__title__popover__image"> <a href="#"><img src="' + media + '"></a> </div> <div class="recent-activities__item2__content__title__popover__text"> <div class="recent-activities__item2__content__title__popover__text__title"> <a href="#">' + businessName + '</a> </div> <div class="recent-activities__item2__content__title__popover__text__star"> <div class="recent-activities__item2__content__title__popover__text__star__icon" data-star="' + rate + '"></div> <div class="recent-activities__item2__content__title__popover__text__star__text"> <p>امتیازات</p> </div> </div> <div class="recent-activities__item2__content__title__popover__text__desc"> <span style="max-width: 200px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' + businessDesc + '</span> </div> </div> </div> </div> <div class="recent-activities__item2__content__image"> <a href="#"><img style="width:98%;margin-right:2px" src="' + media + '"></a> </div> <div class="recent-activities__item2__content__like"> <a href="#"> <svg id="24x24_like_outline" height="24" viewBox="0 0 24 24" width="24"> <path d="M21.164 12.236c.05.164.086.334.086.514 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723A1.75 1.75 0 0 1 18 19H7v-9h1c.37 0 1.257-2.37 2.104-3.345.89-1.017 1.234-1.782 1.457-2.513C11.785 3.412 12 2 12 2s2.388.11 2.388 2.9c0 1.39-.758 3.1-.388 4.1h6.25c.966 0 1.75.784 1.75 1.75 0 .63-.336 1.178-.836 1.486zM20.25 10h-6.946l-.242-.653c-.316-.855-.11-1.862.09-2.835.117-.56.236-1.14.236-1.61 0-.844-.283-1.314-.608-1.577-.076.387-.168.797-.262 1.107-.228.748-.604 1.673-1.66 2.88-.336.386-.744 1.166-1.072 1.794C9.146 10.326 8.796 11 8 11v7h10a.75.75 0 0 0 .75-.75.75.75 0 0 0-.07-.308l-.385-.843.812-.45A.74.74 0 0 0 19.5 15a.75.75 0 0 0-.07-.308l-.385-.843.812-.45a.74.74 0 0 0 .393-.65.793.793 0 0 0-.04-.22l-.23-.74.66-.406A.746.746 0 0 0 20.25 10zM2 10h4v10H2V10z"></path> </svg> لایک </a> </div> </div> </div> </div>');
+	var $data = $('<div class="recent-activities__col"> <div class="recent-activities__item2"> <div class="recent-activities__item2__title"> <div class="recent-activities__item2__title__image"> <a href="#"> <img src="' + personIcon + '"></a> </div> <div class="recent-activities__item2__title__text"> <a href="#">' + personName + '</a> <span> عکس اضافه کرد</span> </div> </div> <div class="recent-activities__item2__content"> <div class="recent-activities__item2__content__title"> <a href="#"> <span>  ' + businessName + ' </span></a> <div class="recent-activities__item2__content__title__popover"> <div class="recent-activities__item2__content__title__popover__image"> <a href="#"><img src="' + media + '"></a> </div> <div class="recent-activities__item2__content__title__popover__text"> <div class="recent-activities__item2__content__title__popover__text__title"> <a href="#">' + businessName + '</a> </div> <div class="recent-activities__item2__content__title__popover__text__star"> <div class="recent-activities__item2__content__title__popover__text__star__icon" data-star="' + rate + '"></div> <div class="recent-activities__item2__content__title__popover__text__star__text"> <p>امتیازات</p> </div> </div> <div class="recent-activities__item2__content__title__popover__text__desc"> <span style="max-width: 200px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' + businessDesc + '</span> </div> </div> </div> </div> <div class="recent-activities__item2__content__image"> <a href="#"><img style="width:98%;margin-right:2px" src="' + media + '"></a> </div> <div class="recent-activities__item2__content__like"> <a href="#"> <svg id="24x24_like_outline" height="24" viewBox="0 0 24 24" width="24"> <path d="M21.164 12.236c.05.164.086.334.086.514 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723A1.75 1.75 0 0 1 18 19H7v-9h1c.37 0 1.257-2.37 2.104-3.345.89-1.017 1.234-1.782 1.457-2.513C11.785 3.412 12 2 12 2s2.388.11 2.388 2.9c0 1.39-.758 3.1-.388 4.1h6.25c.966 0 1.75.784 1.75 1.75 0 .63-.336 1.178-.836 1.486zM20.25 10h-6.946l-.242-.653c-.316-.855-.11-1.862.09-2.835.117-.56.236-1.14.236-1.61 0-.844-.283-1.314-.608-1.577-.076.387-.168.797-.262 1.107-.228.748-.604 1.673-1.66 2.88-.336.386-.744 1.166-1.072 1.794C9.146 10.326 8.796 11 8 11v7h10a.75.75 0 0 0 .75-.75.75.75 0 0 0-.07-.308l-.385-.843.812-.45A.74.74 0 0 0 19.5 15a.75.75 0 0 0-.07-.308l-.385-.843.812-.45a.74.74 0 0 0 .393-.65.793.793 0 0 0-.04-.22l-.23-.74.66-.406A.746.746 0 0 0 20.25 10zM2 10h4v10H2V10z"></path> </svg> لایک </a> </div> </div> </div> </div>');
 	var masonryOptions = {
 		itemSelector: '.recent-activities__col',
 		originLeft: false
@@ -128,4 +126,27 @@ function RecentActivityBusinessMediaSignlePictureCreator(personName, personIcon,
 	$masonry.append($data).masonry('appended', $data);
 	$masonry.masonry('layout');
 }
+function RecentActivityBusinessMediaMultiPictureCreator(personIcon, personName, businessName, businessFirstPicture, rate, description, count, arrImg, businessimg) {
+	var otherimages = "";
+	for (var i = 0; i < arrImg.length; i++) {
+		otherimages += '<div class="recent-activities__item__image__item"><div class="recent-activities__item__image__item__img"><a href="#"><img src=' + arrImg[i] + '></a></div><div class="recent-activities__item__image__item__like"><a href="#" data-toggle="tooltip" title="متن"><svg id="24x24_like_outline" height="24" viewBox="0 0 24 24" width="24"><path d="M21.164 12.236c.05.164.086.334.086.514 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723A1.75 1.75 0 0 1 18 19H7v-9h1c.37 0 1.257-2.37 2.104-3.345.89-1.017 1.234-1.782 1.457-2.513C11.785 3.412 12 2 12 2s2.388.11 2.388 2.9c0 1.39-.758 3.1-.388 4.1h6.25c.966 0 1.75.784 1.75 1.75 0 .63-.336 1.178-.836 1.486zM20.25 10h-6.946l-.242-.653c-.316-.855-.11-1.862.09-2.835.117-.56.236-1.14.236-1.61 0-.844-.283-1.314-.608-1.577-.076.387-.168.797-.262 1.107-.228.748-.604 1.673-1.66 2.88-.336.386-.744 1.166-1.072 1.794C9.146 10.326 8.796 11 8 11v7h10a.75.75 0 0 0 .75-.75.75.75 0 0 0-.07-.308l-.385-.843.812-.45A.74.74 0 0 0 19.5 15a.75.75 0 0 0-.07-.308l-.385-.843.812-.45a.74.74 0 0 0 .393-.65.793.793 0 0 0-.04-.22l-.23-.74.66-.406A.746.746 0 0 0 20.25 10zM2 10h4v10H2V10z"></path></svg>لایک</a></div></div>';
+	}
+
+	var $data = $('<div class="recent-activities__col"><div class="recent-activities__item"><div class="recent-activities__item__title"><div class="recent-activities__item__title__image"><a href="#"> <img src="' + personIcon + '"></a></div><div class="recent-activities__item__title__text"><a href="#">"' + personName + '"</a><span>' + count + 'عکس اضافه کرد</span></div></div><div class="recent-activities__item1__content"><div class="recent-activities__item1__content__title1"><a href="#"><span>' + businessName + '</span></a><div class="recent-activities__item1__content__title1__popover"><div class="recent-activities__item2__content__title__popover__image"><a href="#"><img src="' + businessFirstPicture + '"></a></div><div class="recent-activities__item1__content__title1__popover__text"><div class="recent-activities__item1__content__title1__popover__text__title"><a href="#">' + businessName + '</a></div><div class="recent-activities__item1__content__title1__popover__text__star"><div class="recent-activities__item1__content__title1__popover__text__star__icon" data-star=' + rate + '></div><div class="recent-activities__item1__content__title1__popover__text__star__text"><p>امتیازات</p></div></div><div class="recent-activities__item1__content__title1__popover__text__desc"><span style="max-width: 200px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' + description + '</span></div></div></div></div></div><div class="recent-activities__item__image"><div class="recent-activities__item__image__item recent-activities__item__image__item--big"><div class="recent-activities__item__image__item__img"><a href="#"><img src="' + businessFirstPicture + '"></a></div><div class="recent-activities__item__image__item__like"><a href="#" data-toggle="tooltip" title="متن"><svg id="24x24_like_outline" height="24" viewBox="0 0 24 24" width="24"><path d="M21.164 12.236c.05.164.086.334.086.514 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723 0 .66-.37 1.23-.91 1.527.1.22.16.464.16.723A1.75 1.75 0 0 1 18 19H7v-9h1c.37 0 1.257-2.37 2.104-3.345.89-1.017 1.234-1.782 1.457-2.513C11.785 3.412 12 2 12 2s2.388.11 2.388 2.9c0 1.39-.758 3.1-.388 4.1h6.25c.966 0 1.75.784 1.75 1.75 0 .63-.336 1.178-.836 1.486zM20.25 10h-6.946l-.242-.653c-.316-.855-.11-1.862.09-2.835.117-.56.236-1.14.236-1.61 0-.844-.283-1.314-.608-1.577-.076.387-.168.797-.262 1.107-.228.748-.604 1.673-1.66 2.88-.336.386-.744 1.166-1.072 1.794C9.146 10.326 8.796 11 8 11v7h10a.75.75 0 0 0 .75-.75.75.75 0 0 0-.07-.308l-.385-.843.812-.45A.74.74 0 0 0 19.5 15a.75.75 0 0 0-.07-.308l-.385-.843.812-.45a.74.74 0 0 0 .393-.65.793.793 0 0 0-.04-.22l-.23-.74.66-.406A.746.746 0 0 0 20.25 10zM2 10h4v10H2V10z"></path></svg>لایک</a></div></div>' + otherimages + '</div><div class="recent-activities__item__show-all"><a style="cursor:pointer" hreaf="#">نمایش دیگر عکس ها</a></div></div></div>');
+	var masonryOptions = {
+		itemSelector: '.recent-activities__col',
+		originLeft: false
+	};
+	var $masonry = $('.recent-activities__list').masonry(masonryOptions);
+	$masonry.append($data).masonry('appended', $data);
+	$masonry.masonry('layout');
+	$('.recent-activities__item__show-all').on('click', function () {
+		$(this).siblings('.recent-activities__item__image')
+			.addClass('recent-activities__item__image--active')
+		$(this).remove();
+
+		$masonry.masonry(masonryOptions)
+	});
+}
+
 
