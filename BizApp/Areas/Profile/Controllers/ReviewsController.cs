b50c2f49b4ca05f129fa1f255cf66ddd38f27eb5
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-04-07 13:05:24 +0430

    Start Review

---
+++ BizApp/Areas/Profile/Controllers/ReviewsController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using DataLayer.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BizApp.Areas.Profile.Controllers
+{
+	public class ReviewsController : Controller
+	{
+		private readonly IMapper _mapper;
+		private readonly IUnitOfWorkRepo _unitOfWork;
+
+		public ReviewsController(IUnitOfWorkRepo unitOfWork, IMapper mapper)
+		{
+			_mapper = mapper;
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+	}
+}
